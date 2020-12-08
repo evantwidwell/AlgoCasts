b21deb68df14ce381ca993ec8905c592f3e26370
evantwidwell <evanski@gmail.com>
2020-12-07 19:19:45 -0600

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for(let char of str){
+    if(charMap[char]){
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  }
+  // console.log(charMap)
+  let max = 0;
+  let maxStr = ''
+
+  for(let char in charMap){
+    if(charMap[char] > max){
+      max = charMap[char];
+      maxStr = char
+    }
+  }
+  return maxStr
+}
 
 module.exports = maxChar;
