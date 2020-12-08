fd6f5e63d760059ff5afc5c54927633d688f5a04
evantwidwell <evanski@gmail.com>
2020-12-07 22:20:14 -0600

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let fixedA = stringA.replace(/[^\w]/g,"").toLowerCase().split('').sort().join('');
+  let fixedB = stringB.replace(/[^\w]/g,"").toLowerCase().split('').sort().join('');
+
+  if(fixedA === fixedB){
+    return true
+  } return false
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   let fixedA = stringA.replace(/[^\w]/g,"").toLowerCase().split('');
+//   let fixedB = stringB.replace(/[^\w]/g,"").toLowerCase().split('');
+  
+//   if(fixedA.length !==fixedB.length){
+//     return false
+//   } else{
+
+//   let length = 0;
+//   for(let x=0; x<=fixedB.length; x++){
+//     for(let y=0; y<=fixedB.length; y++ ){
+//       if(fixedA[x] ===fixedB[y]){
+//         length++;
+//         x++;
+//       }
+//     }
+//   }
+//   console.log(length)
+//   if(length === fixedA.length){
+//     return true
+//   }
+//   }
+// }
