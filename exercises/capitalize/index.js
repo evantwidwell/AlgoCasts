248bb210283c1caba3a3e6cbf0abe001f72c71b8
evantwidwell <evanski@gmail.com>
2020-12-08 08:55:48 -0600

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,25 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let sol = str[0].toUpperCase();
+
+  for (let i =1; i<str.length; i++){
+    if(str[i-1] === ' '){
+      sol += str[i].toUpperCase();
+    } else {
+      sol += str[i]
+    }
+  }
+  return sol
+}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   let words = []
+//   for(let word of str.split(' ')){
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return words.join(' ')
+// }
