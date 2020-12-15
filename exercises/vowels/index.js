361fae86aa1f0610bac8331ffc36ca32ad260859
evantwidwell <evanski@gmail.com>
2020-12-14 19:57:08 -0600

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,14 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+let iter = 0;
 
+for (let letter of str.toLowerCase()){
+  if(letter === 'a' || letter ==='e'|| letter === 'i'|| letter ==='o' || letter === 'u'){
+    iter++
+  }
+}
+return iter
+}
 module.exports = vowels;
